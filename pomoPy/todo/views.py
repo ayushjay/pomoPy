929961686829204916f15d52cea37d3bd4e42aa2
Ayush J <ayushjayaswal51@gmail.com>
2025-07-08 16:43:01 +0530

    added login logic and template

--- pomoPy/todo/views.py
+++ pomoPy/todo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
-# Create your views here.
+def registerPage(request):
+    form = CreateUserForm()
+
+
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            
+    context = {"form": form}
+    return render(request,"todo/register.html", context)
+
+
